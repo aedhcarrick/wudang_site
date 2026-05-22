20f24f3302f2e4a08e47b8f748c3edb75e373973
aedhcarrick <aedhcarrick@gmail.com>
2026-05-22 15:32:10 -0500

    added shell script to set python env

--- main_page.py
+++ main_page.py
@@ -5,18 +5,10 @@ import flet as ft
 import os
 import sys
 
-def run_wudang_site():
-    ft.app(
-            target = HomePage,
-            port = 8888,
-            assets_dir = 'assets'
-    )
-
 def HomePage(page: ft.Page):
     page.padding = 0
     page.title = "Wudang Gongfu of New Orleans"
 
-
 if __name__ == '__main__':
-    run_wudang_site()
+    ft.run(HomePage, view=ft.AppView.WEB_BROWSER, port=8080)
 
---
+++ wudang_site.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -i
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+DIRECTORY="."
+ENV_FILE="environment.yaml"
+ENV_NAME="wudang_website"
+ENV_MODIFIED=$(date -r $ENV_FILE "+%s")
+ENV_MODIFIED_FILE=".env_updated"
+ENV_UPDATED=0
+WUDANG_SITE="main_page.py"
+
+INSTALL_ENV_DIR="$(pwd)/../installer_files/env"
+if [ -e "$INSTALL_ENV_DIR" ]; then export PATH="$INSTALL_ENV_DIR/bin:$PATH"; fi
+
+if [[ -f $ENV_MODIFIED_FILE ]]; then
+	ENV_MODIFIED_CACHED=$(<${ENV_MODIFIED_FILE})
+else
+	ENV_MODIFIED_CACHED=0
+fi
+
+conda_env_setup () {
+	CUSTOM_CONDA_PATH=
+
+	if [ -f custom-conda-path.txt ]; then
+		CUSTOM_CONDA_PATH=$(cat custom-conda-path.txt)
+	fi
+	if [ -f "$INSTALL_ENV_DIR/etc/profile.d/conda.sh" ] && [ "$CUSTOM_CONDA_PATH" == ""]; then
+		. "$INSTALL_ENV_DIR/etc/profile.d/conda.sh"
+	fi
+
+	if [ -f "${CUSTOM_CONDA_PATH}/etc/profile.d/conda.sh" ]; then
+		. "${CUSTOM_CONDA_PATH}" ] && [ -f "${CUSTOM_CONDA_PATH}/bin" ]
+		export PATH= "${CUSTOM_CONDA_PATH}/bin:$PATH"
+	fi
+
+	if ! command -v conda >/dev/null; then
+		printf "Conda not found. Install from here --> https://www.anaconda.com/products/distribution \n"
+		exit 1
+	fi
+
+	if ! conda env list | grep ".*${ENV_NAME}.*" >/dev/null 2>&1; then
+		printf "Could not find conda env: ${ENV_NAME} ... creating .. \n\n"
+		conda env create -f $ENV_FILE
+		ENV_UPDATED=1
+	elif [[ ! -z $CONDA_FORCE_UPDATE && $CONDA_FORCE_UPDATE == "true" ]] || (( $ENV_MODIFIED > $ENV_MODIFIED_CACHED )); then
+		printf "Updating conda env: ${ENV_NAME} ...\n\n"
+		PIP_EXISTS_ACTION=w conda env update --file $ENV_FILE --prune
+		ENV_UPDATED=1
+	fi
+
+	if (( $ENV_UPDATED > 0 )); then
+	conda clean --all
+	echo -n $ENV_MODIFIED > $ENV_MODIFIED_FILE
+	fi
+}
+
+conda_env_activation () {
+	conda activate $ENV_NAME
+	conda info | grep active
+}
+
+launch_wudang_site () {
+	python $WUDANG_SITE
+}
+
+init_site () {
+	conda_env_setup
+	conda_env_activation
+	launch_wudang_site
+}
+
+init_site
+
